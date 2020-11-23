9e8ecf72ef8488812ae25c47d2f69bddb45722ac
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-23 12:46:08 +0600

    Worked on Functionality of Sudoku Solver

--- sudoku.py
+++ sudoku.py
@@ -154,3 +154,12 @@ class Grid:
 class Cube:
     rows = 9
     cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
