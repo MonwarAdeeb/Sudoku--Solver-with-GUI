61d899f43293893d74daa7f9c3201447343b8432
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-24 19:30:29 +0600

    Worked on Functionality of Sudoku Solver

--- sudoku.py
+++ sudoku.py
@@ -204,3 +204,11 @@ class Cube:
 
     def set_temp(self, val):
         self.temp = val
+
+    def find_empty(bo):
+        for i in range(len(bo)):
+            for j in range(len(bo[0])):
+                if bo[i][j] == 0:
+                    return (i, j)  # row, col
+
+        return None
