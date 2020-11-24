fbc078cf6439c4802af7acbfd58d59a6778d9eac
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-24 17:05:30 +0600

    Worked on Functionality of Sudoku Solver

--- sudoku.py
+++ sudoku.py
@@ -198,3 +198,6 @@ class Cube:
             pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
         else:
             pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def set(self, val):
+        self.value = val
