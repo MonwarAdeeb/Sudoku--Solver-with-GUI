8b02f672a9721380a3de3f039ffd33ff953f9e6c
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-25 17:17:59 +0600

    Worked on the Functionality of Sudoku Solver

--- sudoku.py
+++ sudoku.py
@@ -236,3 +236,16 @@ def valid(bo, num, pos):
                 return False
 
     return True
+
+
+def redraw_window(win, board, time, strikes):
+    win.fill((255, 255, 255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0, 0, 0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw()
