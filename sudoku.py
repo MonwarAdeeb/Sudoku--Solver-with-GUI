bd3f7d6e8f330829e81073f1bd7b29f2f6f50e19
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-11-26 20:09:05 +0600

    Worked on the Functionality of Sudoku Solver

--- sudoku.py
+++ sudoku.py
@@ -276,3 +276,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
